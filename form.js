b540a53ac7fd33d5e63350f0b972d856d92d0771
rlcoley <rosscoley1002@gmail.com>
2018-09-24 00:29:45 -0400

    setting password to 12345678

--- form.js
+++ form.js
@@ -5,8 +5,16 @@ var submit = document.getElementById("submit")
 
 submit.addEventListener('click', checkForNumber);
 function checkForNumber() {
-    if (userName.includes(1,2,3,5,6,7,8,9,0 ) == false ) {
-      alert("Needs Number")
-    }
+    if (pass === '12345678') {
+      alert("good")
+    }else {
+      alert("Password invaild")
+  }
+
+
+    // if (userName.includes(1,2,3,5,6,7,8,9,0 ) == false ) {
+    //   alert("Needs Number")
+    // }
+
 
 }
